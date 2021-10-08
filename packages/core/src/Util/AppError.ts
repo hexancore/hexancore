3b26a912de602b2567e5d60ce951f7a0b268b583
Mararok <mararok@gmail.com>
2021-10-08 20:06:11 +0200

    small changes

--- packages/core/src/Util/AppError.ts
+++ packages/core/src/Util/AppError.ts
@@ -15,3 +15,10 @@ export interface AppError {
 export function isAppError(value: any): value is AppError {
   return typeof value === 'object' && 'type' in value;
 }
+
+export function IGNORE_ERROR(): AppError {
+  return { type: 'core.ignore_error' };
+}
+export function isIgnoreError(e: AppError): boolean {
+  return e.type === 'core.ignore_error';
+}
