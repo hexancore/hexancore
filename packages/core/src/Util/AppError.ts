13f1b24b2b0bf9fc409fe17c84a37cc2b454d2d7
Mararok <mararok@gmail.com>
2021-11-13 18:28:55 +0100

    small fixes

--- packages/core/src/Util/AppError.ts
+++ packages/core/src/Util/AppError.ts
@@ -1,3 +1,5 @@
+import { HttpStatus } from '@nestjs/common';
+import { error } from './Result';
 /**
  * Represents app error like invalid user input data
  */
@@ -16,9 +18,26 @@ export function isAppError(value: any): value is AppError {
   return typeof value === 'object' && 'type' in value;
 }
 
+const IGNORE_ERROR_TYPE = 'ignore_error';
+
 export function IGNORE_ERROR(): AppError {
-  return { type: 'core.ignore_error' };
+  return { type: IGNORE_ERROR_TYPE };
 }
+
 export function isIgnoreError(e: AppError): boolean {
-  return e.type === 'core.ignore_error';
+  return e.type === IGNORE_ERROR_TYPE;
+}
+
+const INTERNAL_ERROR_TYPE = 'internal_error';
+
+export function INTERNAL_ERROR(error: Error): AppError {
+  return {
+    type: INTERNAL_ERROR_TYPE,
+    code: HttpStatus.INTERNAL_SERVER_ERROR,
+    error,
+  };
+}
+
+export function isInternalError(e: AppError): boolean {
+  return e.type === INTERNAL_ERROR_TYPE;
 }
--- packages/core/src/Util/functions.ts
+++ packages/core/src/Util/functions.ts
@@ -1,3 +1,13 @@
 export function isIterable(obj: any): boolean {
   return typeof obj[Symbol.iterator] === 'function';
 }
+
+export function getClassMethods(obj: Record<string, any>): Array<string> {
+  const properties = new Set<string>();
+  let currentObj = obj;
+  do {
+    Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item));
+  } while ((currentObj = Object.getPrototypeOf(currentObj)));
+
+  return [...properties.keys()].filter((item: string) => typeof obj[item] === 'function');
+}
