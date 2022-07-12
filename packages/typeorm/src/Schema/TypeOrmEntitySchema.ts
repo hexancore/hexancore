1a9b9afc1532e19ba1cd2f89746a624b70c8cc42
Mararok <mararok@gmail.com>
2022-07-12 22:05:57 +0200

    fix tablename

--- packages/typeorm/src/Schema/TypeOrmEntitySchema.ts
+++ packages/typeorm/src/Schema/TypeOrmEntitySchema.ts
@@ -1,15 +1,27 @@
-import { EntityBase, ENTITY_META_PROPERTY } from "@hexcore/core";
-import { EntitySchema, EntitySchemaOptions } from "typeorm";
+import { EntityBase, ENTITY_META_PROPERTY } from '@hexcore/core';
+import { EntitySchema, EntitySchemaColumnOptions, EntitySchemaEmbeddedColumnOptions, EntitySchemaIndexOptions, EntitySchemaOptions } from 'typeorm';
+import { EntitySchemaUniqueOptions } from 'typeorm/entity-schema/EntitySchemaUniqueOptions';
 
-export const TypeOrmEntitySchema = <T extends EntityBase<any>>(entityClass: {new(...args: any[]): T}, options: Pick<EntitySchemaOptions<T>, 'columns'|'indices' | 'embeddeds'>) => {
+type TypeOrmEntitySchemaOptions<T> = {
+  columns: {
+    [P in keyof T]?: EntitySchemaColumnOptions;
+  };
+  indices?: EntitySchemaIndexOptions[];
+  uniques?: EntitySchemaUniqueOptions[];
+  embeddeds?: {
+    [P in keyof Partial<T>]: EntitySchemaEmbeddedColumnOptions;
+  };
+};
+
+export const TypeOrmEntitySchema = <T extends EntityBase<any>>(entityClass: { new (...args: any[]): T }, options: TypeOrmEntitySchemaOptions<T>) => {
   if (!entityClass[ENTITY_META_PROPERTY]) {
-    throw new ReferenceError(`Missing Entity or AggregateRoot decorator on entity class: ${entityClass.name}`)
+    throw new ReferenceError(`Missing Entity or AggregateRoot decorator on entity class: ${entityClass.name}`);
   }
 
   return new EntitySchema<T>({
-  name: entityClass.name,
-  target: entityClass,
-  tableName: entityClass[ENTITY_META_PROPERTY].moduleName + entityClass.name,
-  ...options
+    name: entityClass.name,
+    target: entityClass,
+    tableName: entityClass[ENTITY_META_PROPERTY].module + '_' + entityClass.name,
+    ...options,
   });
 };
